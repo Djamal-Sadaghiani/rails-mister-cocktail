0d937e37fc7d6776c10ceb9cb7c710710e2c59ee
Djamal Sadaghiani <dj8amal@gmail.com>
2018-07-31 15:46:23 +0800

    finished the upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,9 +1,11 @@
 class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :image, presence: true, uniqueness: true
+  validates :image, presence: true
   has_many :doses, :dependent => :destroy
   has_many :ingredients, :through => :doses
 
   include PgSearch
   pg_search_scope :search, against: [:name]
+  mount_uploader :image, ImageUploader
+
 end
